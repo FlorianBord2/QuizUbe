0de8788c84df99544e5a213c280144bc512952cc
Neo <neo@localhost.localdomain>
2021-01-25 13:24:24 +0100

    CHANGE: change api route by get methode

--- sample/quizUbeApi.py
+++ sample/quizUbeApi.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from createQuiz import create_quiz
 from ourYoutube import yapi as ya
 import json
@@ -9,14 +10,19 @@ cq = create_quiz()
 yapi = ya()
 
 
-@app.route('/search_channel/<q>')
-def search_channel(q):
-    res = yapi.youtube_search_channel(q)
-    res = json.dumps(res)
-    return res
+@app.route('/search_channel', methods=['GET'])
+def search_channel():
+    if request.method == 'GET':
+        res = yapi.youtube_search_channel(request.args['q'])
+        res = json.dumps(res)
+        return res
+    else:  
+        return 'Wrong method'
 
-
-@app.route('/get_quiz/<channelId>')
-def get_quiz(channelId):
-    res = cq.create(channelId)
-    return str(res)
+@app.route('/get_quiz', methods=['GET'])
+def get_quiz():
+    if request.method == 'GET':
+        res = cq.create(request.args['channelId'])
+        return res
+    else:
+        return 'Wrong method'
