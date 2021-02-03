7e7c7fbffdb9735a43e72f12f50cb83966f8e35e
Alexis Gomes <tehdyze@gmail.com>
2021-02-04 00:29:34 +0100

    First front commit

---
+++ Front/QuizUbeXamarin/QuizUbeXamarin/QuizUbeXamarin/Classes/ChannelResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizUbeXamarin.Classes
+{
+	class ChannelResult
+	{
+		public string Title;
+		public string ChannelID;
+		public string URL;
+	}
+}
