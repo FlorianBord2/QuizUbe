5d583efbf78de92773aa534f21c10f4989166842
Misterpixel <alexis.gomesfr@gmail.com>
2021-02-24 01:26:22 +0100

    New front

---
+++ Front/QuizUbeXamarin/Quizube/Quizube/Assets/Scripts/ChannelResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChannelResultData
+{
+	public string Title;
+	public string ChannelID;
+	public string URL;
+}
+
+public class ChannelResult : MonoBehaviour
+{
+	public ChannelResultData ChannelResultData;
+
+	public Text ChannelNameText;
+	public Text ChannelSubsText;
+	public Image ChannelPic;
+
+	public void Init(ChannelResultData data)
+	{
+		ChannelNameText.text = data.Title;
+	}
+}
---
+++ Front/QuizUbeXamarin/Quizube/Quizube/Assets/Scripts/WebUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class WebUtility : MonoBehaviour
+{
+	private static WebUtility _instance;
+	public static WebUtility Instance => _instance;
+
+	private void Awake()
+	{
+		_instance = this;
+	}
+
+	public string Get(string uri)
+	{
+		HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+		request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+		using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+		using (Stream stream = response.GetResponseStream())
+		using (StreamReader reader = new StreamReader(stream))
+		{
+			return reader.ReadToEnd();
+		}
+	}
+
+	public IEnumerator GetTexture(string url, Action<Texture> textureDownloadedCallback)
+	{
+		UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+		yield return www.SendWebRequest();
+
+		textureDownloadedCallback?.Invoke(DownloadHandlerTexture.GetContent(www));
+	}
+}
